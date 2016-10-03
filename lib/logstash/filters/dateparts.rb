d740611076be5d55ee1e00e747ecf50b9e98a0ab
mike baranski <mike.baranski@gmail.com>
2016-10-02 20:39:31 -0400

    Updated code style; string quotes, remove concatenation, use %w, etc...

--- lib/logstash/filters/dateparts.rb
+++ lib/logstash/filters/dateparts.rb
@@ -1,4 +1,4 @@
-# coding: utf-8
+# encoding: utf-8
 # Copyright (c) 2014–2015 Mike Baranski <http://www.mikeski.net>
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 # encoding: utf-8
-require "logstash/filters/base"
-require "logstash/namespace"
+require 'logstash/filters/base'
+require 'logstash/namespace'
 
 # This filter will add date parts to your record based on
 # the timestamp field.
@@ -30,35 +30,35 @@ class LogStash::Filters::DateParts < LogStash::Filters::Base
   #   }
   # }
   #
-  config_name "dateparts"
-  config :fields, :validate => :array, :default => ["day", "wday", "yday", "month", "year", "hour", "min", "sec"], :required => true
-  config :time_field, :validate => :string, :default => "@timestamp", :required => true
-  config :error_tags, :validate => :array, :default => ["_dateparts_error"], :required => true
-  
+  config_name 'dateparts'
+  config :fields, :validate => :array, :default => %w(day wday yday month year hour min sec), :required => true
+  config :time_field, :validate => :string, :default => '@timestamp', :required => true
+  config :error_tags, :validate => :array, :default => ['_dateparts_error'], :required => true
+
   public
   def register
-    logger.debug? and logger.debug("DateParts filter registered")
+    logger.debug? and logger.debug('DateParts filter registered')
   end
 
   def plugin_error(message, event)
-    logger.error("DatePart filter error: " + message)
+    logger.error("DatePart filter error: #{message}")
     LogStash::Util::Decorators.add_tags(@error_tags, event, "filters/#{self.class.name}")
   end
 
   def get_time_from_field(f)
     if f.class == Time
-      return f
-    elsif f.respond_to?("time")
-      return f.time()
+      f
+    elsif f.respond_to?('time')
+      f.time
     else
-      return nil
+      nil
     end
   end
-  
+
   public
   def filter(event)
-    if @fields.respond_to?("each") and @fields.respond_to?("join")
-      logger.debug? and logger.debug("DateParts plugin filtering #{@time_field} time_field and adding fields: " + @fields.join(", "))
+    if @fields.respond_to?('each') and @fields.respond_to?('join')
+      logger.debug? and logger.debug("DateParts plugin filtering #{@time_field} time_field and adding fields: " + @fields.join(', '))
       t = get_time_from_field(event.get(@time_field))
       if t == nil
         plugin_error("Invalid time field #{@time_field}; Time field must be an instance of Time or provide a time method that returns one", event)
@@ -72,11 +72,11 @@ class LogStash::Filters::DateParts < LogStash::Filters::Base
         end
       end
     else
-      plugin_error("DateParts plugin fields invalid, should be an array of function names")
+      plugin_error('DateParts plugin fields invalid, should be an array of function names', event)
       return
     end
-      
+
     filter_matched(event)
   end # def filter
-  
+
 end # class LogStash::Filters::DateParts
